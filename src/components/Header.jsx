eb4614f77cd55f9b5fd134d9103e9a3dcb9145c4
Kovryga Kirill <kovrygakirill@mail.ru>
2021-02-16 10:55:00 +0300

    realization css grid

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://webcomicms.net/sites/default/files/clipart/170473/cool-logos-draw-170473-5461799.jpg'/>
+        </header>
+    );
+}
+
+export default Header;
