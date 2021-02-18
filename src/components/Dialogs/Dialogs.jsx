43067b97946e1bc85c7fc91aa0c3ec79765b9ba0
Kovryga Kirill <kovrygakirill@mail.ru>
2021-02-18 20:11:18 +0300

    create page Dialogs

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import dlog from './Dialogs.module.css';
+
+const Dialogs = (props) => {
+    return (
+        <div>
+            Daliogs
+        </div>
+    );
+}
+
+export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from "./My_posts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={pfile.content}>
+        <div>
             <div>
                 <img
                     src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'/>
