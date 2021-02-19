3450ce578f2bd84a43e4120f48e9bb3710f37c97
Kovryga Kirill <kovrygakirill@mail.ru>
2021-02-19 16:15:57 +0300

    layout Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,27 @@ import dlog from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Daliogs
+        <div className={dlog.dialogs}>
+            <div className={dlog.dialogsItem}>
+                <div className={dlog.dialog + " " + dlog.active}>
+                    Dimych
+                </div>
+                <div className={dlog.dialog}>
+                    Kirill
+                </div>
+                <div className={dlog.dialog}>
+                    Sveta
+                </div>
+                <div className={dlog.dialog}>
+                    Max
+                </div>
+            </div>
+
+            <div className={dlog.messages}>
+                <div className={dlog.message}>Hi</div>
+                <div className={dlog.message}>How are you?</div>
+                <div className={dlog.message}>What is your name?</div>
+            </div>
         </div>
     );
 }
