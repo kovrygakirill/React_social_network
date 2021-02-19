2113bf2b4ac76cf367684245c74f4ab61116082b
Kovryga Kirill <kovrygakirill@mail.ru>
2021-02-19 15:48:06 +0300

    use NavLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import nbar from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={nbar.nav}>
             <div className={nbar.item}>
-                <a href='/profile'>Profile</a>
+                <NavLink to='/profile' activeClassName={nbar.activeLink}>Profile</NavLink>
             </div>
-            <div className={`${nbar.item} ${nbar.active}`}>
-                <a href='/dialog'>Messages</a>
+            <div className={nbar.item}>
+                <NavLink to='/dialog' activeClassName={nbar.activeLink}>Messages</NavLink>
             </div>
             <div className={nbar.item}>
                 <a>News</a>
